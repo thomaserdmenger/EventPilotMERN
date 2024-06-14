d14dd8b390cf10b94a42aae9b6a8729379d5deff
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-06-14 15:02:21 +0200

    add folder to cloudinary func

--- backend/src/users/users.controller.js
+++ backend/src/users/users.controller.js
@@ -1,7 +1,14 @@
-export const patchUser = async (req, res) => {
+export const patchUserCtrl = async (req, res) => {
   try {
+    // req.body => zu ändernde Daten
+
     const image = req.file;
     // hier Aufruf von uploadImage(image.buffer) in Abhängigkeit davon, ob ein image existiert
+    const uploadResult = image
+      ? await uploadProfileImage(image.buffer)
+      : undefined;
+    // hier alle weiteren Funktionen, um den User zu speichern
+    // und uploadResult.secure_url als imageProfile im user speichern
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message || "Could not edit user." });
