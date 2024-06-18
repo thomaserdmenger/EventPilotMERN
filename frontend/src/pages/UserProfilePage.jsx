a0805a5ddf7d89f34621a314266e678afb1f1c60
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-06-18 08:02:00 +0200

    Add Edit Button and navigate to userprofileedit

--- frontend/src/pages/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage.jsx
@@ -2,10 +2,15 @@ import HeaderNav from "../components/HeaderNav";
 import { useContext, useEffect, useState } from "react";
 import { backendUrl } from "../api/api";
 import { UserContext } from "../context/UserContext";
+import Button from "@mui/material/Button";
+import { FaRegEdit } from "react-icons/fa";
+import CustomButton from "../components/CustomButton";
+import { useNavigate } from "react-router-dom";
 
 const UserProfilePage = () => {
   const { user } = useContext(UserContext);
   const [followers, setFollowers] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Get users, that follow the auth user
@@ -20,12 +25,16 @@ const UserProfilePage = () => {
     fetchData();
   }, []);
 
+  const editProfile = () => {
+    navigate("/userprofileedit");
+  };
+
   return (
     <>
       <HeaderNav />
       <div>
         <p>UserImage</p>
-        <article className="flex gap-6 justify-center items-center">
+        <article className="flex gap-6 justify-center items-center mb-9">
           <div className="flex flex-col items-center text-center">
             <p>{user?.followedUsers?.length}</p>
             <p className="text-grey-2 font-roboto-thin text-[14px]">Following</p>
@@ -36,6 +45,21 @@ const UserProfilePage = () => {
             <p className="text-grey-2 font-roboto-thin text-[14px]">Followers</p>
           </div>
         </article>
+        <div className="flex justify-center">
+          <CustomButton
+            fontSize={"16px"}
+            color={"#7254EE"}
+            width={"50%"}
+            borderRadius={"15px"}
+            bgcolor={"#ffffff"}
+            bgcolorHover={"#ffffff"}
+            padding={"16px"}
+            text={"Edit Profile"}
+            border={"1px solid #7254EE"}
+            endIcon={<FaRegEdit />}
+            onClick={editProfile}
+          />
+        </div>
       </div>
     </>
   );
