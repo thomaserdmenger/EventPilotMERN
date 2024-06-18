fa34117ad51e5c16be5cdd6b56e87b2121d56127
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-06-18 08:29:14 +0200

    Add User Interests on User Profile Page

--- frontend/src/pages/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage.jsx
@@ -2,10 +2,10 @@ import HeaderNav from "../components/HeaderNav";
 import { useContext, useEffect, useState } from "react";
 import { backendUrl } from "../api/api";
 import { UserContext } from "../context/UserContext";
-import Button from "@mui/material/Button";
 import { FaRegEdit } from "react-icons/fa";
 import CustomButton from "../components/CustomButton";
 import { useNavigate } from "react-router-dom";
+import { categories } from "../constants/categories.js";
 
 const UserProfilePage = () => {
   const { user } = useContext(UserContext);
@@ -33,7 +33,9 @@ const UserProfilePage = () => {
     <>
       <HeaderNav />
       <div>
-        <p>UserImage</p>
+        <div className=" flex justify-center mb-[40px] mt-6">
+          <p>UserImage</p>
+        </div>
         <article className="flex gap-6 justify-center items-center mb-9">
           <div className="flex flex-col items-center text-center">
             <p>{user?.followedUsers?.length}</p>
@@ -49,20 +51,32 @@ const UserProfilePage = () => {
           <CustomButton
             fontSize={"16px"}
             color={"#7254EE"}
-            width={"50%"}
+            width={"40%"}
             borderRadius={"15px"}
             bgcolor={"#ffffff"}
             bgcolorHover={"#ffffff"}
-            padding={"16px"}
+            padding={"14px"}
             text={"Edit Profile"}
             border={"1px solid #7254EE"}
             endIcon={<FaRegEdit />}
             onClick={editProfile}
           />
         </article>
-        <article className="px-8">
+        <article className="px-8 mb-9">
           <h2 className="text-[18px] mb-[10px] font-roboto-medium">About me</h2>
           <p className="text-grey-2 font-roboto-thin">{user?.user?.bio}</p>
+        </article>
+        <article className="px-8 mb-9">
+          <h2 className="text-[18px] mb-[10px] font-roboto-medium">Interest</h2>
+          <div className="flex gap-2 flex-wrap">
+            {user?.user?.interests?.sort().map((item, index) => {
+              return (
+                <p key={index} className="bg-purple-1 text-white rounded-md px-3 py-1 text-[13px]">
+                  {item}
+                </p>
+              );
+            })}
+          </div>
         </article>
       </div>
     </>
