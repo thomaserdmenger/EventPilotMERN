7a31717b5331e946109c64999fa984e2c6ac4f13
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-06-19 16:05:47 +0200

    get place name from lat/long

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,11 +1,14 @@
+import CurrentLocation from "../components/CurrentLocation";
+
 const SearchPage = () => {
   return (
     <div className="h-svh">
       <div className="pb-[4.375rem]">
         <h1>Search</h1>
+        <CurrentLocation />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
