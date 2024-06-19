ba8c413516975be5cccd7ee3625eed6f0e6a0a33
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-06-19 09:49:32 +0200

    Update Styling for Textarea on User Profile Page

--- frontend/src/components/CustomTextArea.jsx
+++ frontend/src/components/CustomTextArea.jsx
@@ -1,40 +1,40 @@
-import { styled } from '@mui/material'
-import TextField from '@mui/material/TextField'
+import { styled } from "@mui/material";
+import TextField from "@mui/material/TextField";
 
 const StyledTextField = styled(TextField)(({ theme }) => ({
-  '& .MuiOutlinedInput-root': {
-    position: 'relative',
-    '& fieldset': {
-      borderColor: '#7254EE',
-      borderRadius: '16px',
+  "& .MuiOutlinedInput-root": {
+    position: "relative",
+    "& fieldset": {
+      borderColor: "#7254EE",
+      borderRadius: "16px",
     },
-    '&:hover fieldset': {
-      borderColor: '#7254EE',
+    "&:hover fieldset": {
+      borderColor: "#7254EE",
     },
-    '&.Mui-focused fieldset': {
-      borderColor: '#00ECAA',
+    "&.Mui-focused fieldset": {
+      borderColor: "#00ECAA",
     },
   },
-  '& .MuiInputLabel-root': {
-    color: '#7254EE',
-    position: 'absolute',
+  "& .MuiInputLabel-root": {
+    color: "#7254EE",
+    position: "absolute",
     top: theme.spacing(-1),
     left: theme.spacing(1),
-    transform: 'translate(0, 0) scale(1)',
-    pointerEvents: 'none',
-    backgroundColor: '#FFFFFF',
-    padding: '0 6px',
-    fontSize: '12px',
+    transform: "translate(0, 0) scale(1)",
+    pointerEvents: "none",
+    backgroundColor: "#FFFFFF",
+    padding: "0 6px",
+    fontSize: "12px",
   },
-  '& .MuiInputBase-input': {
-    color: '#7254EE',
+  "& .MuiInputBase-input": {
+    color: "#7254EE",
   },
-  '& .MuiInputLabel-root.Mui-focused': {
-    color: '#7254EE',
+  "& .MuiInputLabel-root.Mui-focused": {
+    color: "#7254EE",
   },
-}))
+}));
 
-const CustomTextArea = ({ label, onChange, value }) => {
+const CustomTextArea = ({ label, onChange, value, name }) => {
   return (
     <>
       <StyledTextField
@@ -44,13 +44,14 @@ const CustomTextArea = ({ label, onChange, value }) => {
         multiline
         rows={4}
         label={label}
+        name={name}
         InputLabelProps={{ shrink: true }}
         sx={{
-          width: '100%',
+          width: "100%",
         }}
       />
     </>
-  )
-}
+  );
+};
 
-export default CustomTextArea
+export default CustomTextArea;
