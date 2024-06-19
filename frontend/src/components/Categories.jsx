d91db7c1c2010c824cf282b258169db62f2939a0
icaroboaventura <boaventura@outlook.de>
2024-06-19 13:56:15 +0200

    Add categories

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,37 +1,39 @@
-import { categories } from "../constants/categories";
+import { categories } from '../constants/categories'
 
 const Categories = ({ categoriesArray, setCategoriesArray }) => {
   // check if clicked category is already part of array -> return boolean
-  const checkCategoriesArray = (category) => {
-    return categoriesArray?.some((item) => category === item);
-  };
+  const checkCategoriesArray = category => {
+    return categoriesArray?.some(item => category === item)
+  }
 
   // depending on checked click: either add or delete category from array -> return updated array
-  const handleCatArray = (category) => {
-    const isClicked = checkCategoriesArray(category);
+  const handleCatArray = category => {
+    const isClicked = checkCategoriesArray(category)
     if (isClicked) {
       // wenn true: filtern und state neu setzen
-      const filteredArray = categoriesArray.filter((item) => item !== category);
-      return setCategoriesArray(filteredArray);
+      const filteredArray = categoriesArray.filter(
+        item => item !== category,
+      )
+      return setCategoriesArray(filteredArray)
     } else if (!isClicked) {
       // wenn false: spread und speichern
-      return setCategoriesArray([...categoriesArray, category]);
+      return setCategoriesArray([...categoriesArray, category])
     }
-  };
+  }
   return (
     <div className="border border-[#7254EE] rounded-[16px] p-4 relative">
       <h3 className="absolute top-[-10px] text-[#7254EE] text-[12px] bg-white px-1 ml-[-7px] font-roboto-regular">
         Categories
       </h3>
-      <div className="flex flex-wrap gap-2">
-        {categories?.map((singleCategory) => (
+      <div className="flex flex-wrap gap-4 items-center justify-center">
+        {categories?.map(singleCategory => (
           <p
             onClick={() => handleCatArray(singleCategory)}
-            className={
-              checkCategoriesArray(singleCategory)
-                ? "text-[#7254EE] cursor-pointer"
-                : "text-[#9CA3AF] cursor-pointer"
-            }
+            className={`min-w-20 text-center py-1 px-2 text-sm rounded-[1rem] border-purple-2 border font-roboto-regular cursor-pointer ${
+              checkCategoriesArray(singleCategory) ?
+                'text-white bg-purple-2 '
+              : ' text-grey-2  '
+            }`}
             key={singleCategory}
             name={singleCategory}>
             {singleCategory}
@@ -39,7 +41,7 @@ const Categories = ({ categoriesArray, setCategoriesArray }) => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Categories;
+export default Categories
