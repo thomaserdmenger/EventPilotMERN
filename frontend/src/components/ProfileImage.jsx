bada2a7ca4c4d583934540e431c313ef26713b4c
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-06-20 14:21:52 +0200

    fix profileImage on explorePage

--- frontend/src/components/ProfileImage.jsx
+++ frontend/src/components/ProfileImage.jsx
@@ -1,19 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const ProfileImage = ({ src, className, to }) => {
+  const { pathname } = useLocation();
   return (
     <>
-      <Link to={to}>
-        {src ? (
-          <img src={src} className={className} alt="User Image" />
-        ) : (
-          <img
-            className={className}
-            src="/images/avatar_default.png"
-            alt="User Image"
-          />
-        )}
-      </Link>
+      {pathname === "/" ? (
+        <div>
+          {src ? (
+            <img src={src} className={className} alt="User Image" />
+          ) : (
+            <img
+              className={className}
+              src="/images/avatar_default.png"
+              alt="User Image"
+            />
+          )}
+        </div>
+      ) : (
+        <Link to={to}>
+          {src ? (
+            <img src={src} className={className} alt="User Image" />
+          ) : (
+            <img
+              className={className}
+              src="/images/avatar_default.png"
+              alt="User Image"
+            />
+          )}
+        </Link>
+      )}
     </>
   );
 };
