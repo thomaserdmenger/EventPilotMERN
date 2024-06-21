00ae33f75e2a29d21140171a243e637b0ae5abe5
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-06-21 09:50:18 +0200

    Filter Data with Categories and Search Term by fantastic Mia

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -3,11 +3,13 @@ import CurrentLocation from "../components/CurrentLocation";
 import SearchLocation from "../components/SearchLocation";
 import { backendUrl } from "../api/api";
 import EventCardSmall from "../components/EventCardSmall";
+import { categories } from "../constants/categories.js";
 
 const SearchPage = () => {
   const [searchText, setSearchText] = useState("");
   const [eventsData, setEventsData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -33,9 +35,39 @@ const SearchPage = () => {
     setFilteredData(filteredData);
   };
 
+  const handleCategories = (e) => {
+    const isClicked = selectedCategory === e.target.textContent;
+
+    if (isClicked) {
+      setSelectedCategory("");
+      if (searchText)
+        setFilteredData(
+          eventsData?.filter((item) => item?.title.toLowerCase().includes(searchText))
+        );
+      return;
+    }
+
+    setSelectedCategory(e.target.textContent);
+    const selectedCategoryConst = e.target.textContent;
+
+    const filteredCategories =
+      filteredData.length < 1
+        ? eventsData?.filter((item) =>
+            item?.categories?.find((item) => item === selectedCategoryConst)
+          )
+        : filteredData?.filter((item) =>
+            item?.categories?.find((item) => item === selectedCategoryConst)
+          );
+
+    setFilteredData(filteredCategories);
+
+    // setSelectedCategory([...selectedCategories, e.target.textContent]);
+  };
+
   return (
     <div className="flex flex-col items-center justify-start h-svh">
       <div className="pb-[4.375rem]">{/* <CurrentLocation /> */}</div>
+      {/* Search Input Field */}
       <input
         onChange={handleSearch}
         value={searchText}
@@ -43,14 +75,29 @@ const SearchPage = () => {
         type="text"
         placeholder="Search"
       />
+      {/* Categories Filter */}
+      <div className="flex gap-2 flex-wrap justify-center mb-8">
+        {categories?.map((cat, index) => {
+          return (
+            <div onClick={handleCategories} className="border-[1px] p-1" key={index}>
+              <p className={selectedCategory === cat ? `text-blue-500` : "text-red-500"}>{cat}</p>
+            </div>
+          );
+        })}
+      </div>
+      {/* Small Cards */}
       <div className="px-8">
-        {filteredData.length > 0
-          ? filteredData?.map((item) => {
-              return <EventCardSmall key={item?._id} event={item} />;
-            })
-          : eventsData?.map((item) => {
-              return <EventCardSmall key={item?._id} event={item} />;
-            })}
+        {filteredData.length < 1 && searchText.length > 0 ? (
+          <p>Nothing found.</p>
+        ) : filteredData.length > 0 ? (
+          filteredData?.map((item) => {
+            return <EventCardSmall key={item?._id} event={item} />;
+          })
+        ) : (
+          eventsData?.map((item) => {
+            return <EventCardSmall key={item?._id} event={item} />;
+          })
+        )}
       </div>
     </div>
   );
@@ -59,6 +106,6 @@ const SearchPage = () => {
 export default SearchPage;
 
 // ! Was brauchen wir alles?
-// - Upcoming Events fetchen => State für upcomming Events
-// - Filten nach Event Name
-// - Render in Small Cards
+// - Categories aus JS Rendern
+// - Ausgewählte Kategorien in State(s) speichern
+// - FilteredData beeinflussen oder EventsData => Ergebnis in Filtered speichern
