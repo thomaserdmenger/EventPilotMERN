4a679af02234ec0a168a536d3e30634b7feffd97
icaroboaventura <boaventura@outlook.de>
2024-06-24 11:44:56 +0200

    Add many things

--- frontend/src/components/CurrentLocation.jsx
+++ frontend/src/components/CurrentLocation.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import MyLocationIcon from '@mui/icons-material/MyLocation'
+import { CircularProgress } from '@mui/material'
 
 const CurrentLocation = ({
   borderFind,
@@ -8,37 +9,42 @@ const CurrentLocation = ({
   setLocalCity,
 }) => {
   const [city, setCity] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const getMyLocation = () => {
-    // get lat/long and search in Google Api:
+    setLoading(true)
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showCity)
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const latitude = position.coords.latitude
+          const longitude = position.coords.longitude
+          fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${
+              import.meta.env.VITE_GOOGLE_API_KEY
+            }`,
+          )
+            .then(response => response.json())
+            .then(data => {
+              const city = data.results[0].address_components.find(
+                component => component.types.includes('locality'),
+              ).long_name
+              setLocalCity(city)
+              setCity(city)
+              setLoading(false)
+            })
+            .catch(error => {
+              console.log(error)
+              setLoading(false)
+            })
+        },
+        error => {
+          console.log(error)
+          setLoading(false)
+        },
+      )
     } else {
       console.log('Geolocation is not supported by this browser.')
-    }
-
-    // Then, pass the location coordinates to a Geocoding API to get the city name
-    function showCity(position) {
-      const latitude = position.coords.latitude
-      const longitude = position.coords.longitude
-
-      // Make a request to a Geocoding API (e.g. Google Maps Geocoding API)
-      fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${
-          import.meta.env.VITE_GOOGLE_API_KEY
-        }`,
-      )
-        .then(response => response.json())
-        .then(data => {
-          // Parse the city name from the API response (to find under "locality")
-          const city = data.results[0].address_components.find(component =>
-            component.types.includes('locality'),
-          ).long_name
-
-          setLocalCity(city)
-          setCity(city)
-        })
-        .catch(error => console.log(error))
+      setLoading(false)
     }
   }
 
@@ -49,14 +55,19 @@ const CurrentLocation = ({
       </p>
       {city ?
         <p
-          className={`text-[14px] ${borderFind} ${textColorFind}  border-2 px-2 py-1 rounded-[15px]`}>
+          className={`text-[14px] ${borderFind} ${textColorFind} border-2 px-2 py-1 rounded-[15px]`}>
           {city}
         </p>
       : <div
-          className={`flex gap-2 items-center cursor-pointer ${borderFind} ${textColorFind}  border-2 px-2 py-1 rounded-[15px] `}
+          className={`flex gap-2 items-center cursor-pointer ${borderFind} ${textColorFind} border-2 px-2 py-1 rounded-[15px] `}
           onClick={getMyLocation}>
-          <MyLocationIcon className="e" style={{ fontSize: 15 }} />
-          <p className={`text-[14px]`}>Find my location </p>
+          {loading ?
+            <CircularProgress size={15} style={{ color: '#777BF3' }} />
+          : <>
+              <MyLocationIcon className="e" style={{ fontSize: 15 }} />
+              <p className="text-[14px]">Find my location </p>
+            </>
+          }
         </div>
       }
     </div>
